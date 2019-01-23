c3d94465184348e6050d84bcecc62698021d256a
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-23 08:56:08 -0500

    Added Comments
    Describing an Object Initializer and its usage

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace ObjectInitializers
+﻿using System;
+
+namespace ObjectInitializers
 {
     class Program
     {
+        // Object Initializer:  Allows us to intialize an object without the need to call one of its constructors.
+        //  -- Allows us the ability to avoid creating multiple constructors which can become difficult to manage.
         static void Main(string[] args)
         {
         }
