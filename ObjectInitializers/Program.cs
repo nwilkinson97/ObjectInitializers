1336301d7fcf74306be37f74a7d9e43a51bf3cae
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-23 08:57:58 -0500

    Add New Class Person
    Added a New Class Person with some basic fields.  Note:  that the type DateTime requires the System Namespace.  After typing DateTime you can use the Quick Actions shortcut {CTRL} + .    to add the appropriate namespace to the top of the file.

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -2,6 +2,14 @@
 
 namespace ObjectInitializers
 {
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public DateTime BirthDate;
+    }
+
     class Program
     {
         // Object Initializer:  Allows us to intialize an object without the need to call one of its constructors.
