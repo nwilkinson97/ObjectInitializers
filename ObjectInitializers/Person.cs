cf520f8ce85bc1762f22e5215a029a8d69c241df
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-23 08:59:27 -0500

    Moved Person Class to New File
    Using the Quick Actions shortcut {CTRL} + .  you can quickly move the Person class to a file with the same name as the class.

---
+++ ObjectInitializers/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectInitializers
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public DateTime BirthDate;
+    }
+}
--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -1,15 +1,5 @@
-﻿using System;
-
-namespace ObjectInitializers
+﻿namespace ObjectInitializers
 {
-    public class Person
-    {
-        public int Id;
-        public string FirstName;
-        public string LastName;
-        public DateTime BirthDate;
-    }
-
     class Program
     {
         // Object Initializer:  Allows us to intialize an object without the need to call one of its constructors.
