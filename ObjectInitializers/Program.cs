873a94fd70327864acf92d947760812f63ddfae7
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 07:40:23 -0500

    Example using Object Intialization

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -6,7 +6,10 @@
         //  -- Allows us the ability to avoid creating multiple constructors which can become difficult to manage.
         static void Main(string[] args)
         {
-
+            // An example of how Object Initialization can be used on the Person Class.  
+            // Here during the initialization process we define the fields that we know
+            // without the use of the class constructors.
+            var person = new Person { FirstName = "Super", LastName = "Thing" };
         }
     }
 }
